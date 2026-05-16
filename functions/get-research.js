df43ae64f7553dcaf35f7c74c53264151c894d27
Abd'ul Haseeb <alhaseeb2006@gmail.com>
2026-05-16 22:08:28 +0500

    Delete functions/get-research.js

--- functions/get-research.js
+++
@@ -1,7 +0,0 @@
-export async function onRequestPost(context) {
-  const { request, env } = context;
-  const { email } = await request.json();
-  const logs = await env.DB.prepare("SELECT topic, summary, date_consulted FROM research_logs WHERE user_email = ?")
-    .bind(email).all();
-  return new Response(JSON.stringify(logs.results), { headers: {"Content-Type":"application/json"} });
-}
